d775ec274c64063860508f3dcec0408a5bf68e7d
drishaanortho <ishaanshehra@gmail.com>
2026-07-12 20:50:09 +0530

    Add Services Page

---
+++ app/services/page.tsx
@@ -0,0 +1,20 @@
+export default function ServicesPage() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Our Services</h1>
+
+      <ul>
+        <li>Joint Replacement Surgery</li>
+        <li>Arthroscopy & Sports Injury Treatment</li>
+        <li>Shoulder Surgery</li>
+        <li>Spine Disorders</li>
+        <li>Trauma & Fracture Management</li>
+        <li>Arthritis Treatment</li>
+        <li>Back & Neck Pain Management</li>
+        <li>Pediatric Orthopaedics</li>
+        <li>CTEV (Clubfoot) Treatment</li>
+        <li>Nonunion & Malunion Surgery</li>
+      </ul>
+    </main>
+  );
+}
