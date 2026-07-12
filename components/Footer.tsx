14f0f737f51a141f2fce0e2d829af7a6ac1739b6
drishaanortho <ishaanshehra@gmail.com>
2026-07-12 21:06:51 +0530

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#0f766e",
+        color: "white",
+        textAlign: "center",
+        padding: "30px 20px",
+        marginTop: "40px",
+      }}
+    >
+      <h2>Dr. Ishaan Meena Orthopaedic Clinic</h2>
+
+      <p>MBBS | MS (Orthopaedics)</p>
+      <p>Assistant Professor, JLN Medical College, Ajmer</p>
+
+      <p style={{ marginTop: "15px" }}>
+        © 2026 Dr. Ishaan Meena. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
