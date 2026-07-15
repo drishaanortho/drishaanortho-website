fdd059ce743ce53c0c4c26517458535417a2fdcc
drishaanortho <ishaanshehra@gmail.com>
2026-07-15 15:27:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import "./globals.css";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 export const metadata = {
   title: "Dr. Ishaan Meena Orthopaedic Clinic",
@@ -15,10 +13,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-  <Navbar />
-  {children}
-  <Footer />
-</body>
+        {children}
+      </body>
     </html>
   );
 }
