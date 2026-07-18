b47a4ac145e3e80fb7cab7a39a3b61c020a56cca
drishaanortho <ishaanshehra@gmail.com>
2026-07-18 12:34:33 +0530

    Refactor Services section into reusable component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "../components/Hero";
 import About from "../components/About";
 import WhyChooseUs from "../components/WhyChooseUs";
-import Services from "@/components/Services";
+import Services from "../components/Services";
 export default function Home() {
   
   return (
